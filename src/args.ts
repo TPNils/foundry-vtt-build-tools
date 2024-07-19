1264db12a8caeaa0a0571e89430ea904dac09915
Nils <nils.yseboodt@gmail.com>
2024-07-19 12:52:08 +0200

    fix: parsing args (& remove yargs dependancy)

--- src/args.ts
+++ src/args.ts
@@ -1,15 +1,27 @@
 import { Version } from './version.js';
-import * as chalk from 'chalk';
-import * as yargs from 'yargs';
+import chalk from 'chalk';
 
 export class Args {
-  private static args: {
-    u?: string; update?: string;
-    fi?: string; foundryinstance?: string;
-  } = yargs.argv;
+
+  static #getArg(short: string, long: string): string | null {
+    short = short.replace(/^-{0,1}/, '-');
+    long = long.replace(/^-{0,2}/, '--');
+    for (let i = 0; i < process.argv.length; i++) {
+      const arg = process.argv[i];
+      if (arg === short || arg === long) {
+        return process.argv[i+1]
+      } else if (arg.startsWith(short + '=')) {
+        return arg.substring(short.length);
+      } else if (arg.startsWith(long + '=')) {
+        return arg.substring(long.length);
+      }
+    }
+
+    return null;
+  }
  
   public static getNextVersion(currentVersion: Version): Version {
-    const version = this.args.update || this.args.u;
+    const version = Args.#getArg('u', 'update');
     if (!version) {
       throw new Error('Missing version number. Use -u <version> (or --update) to specify a version.');
     }
@@ -44,6 +56,6 @@ export class Args {
   }
   
   public static getFoundryInstanceName(): string | undefined {
-    return this.args.foundryinstance ?? this.args.fi;
+    return Args.#getArg('fi', 'foundryinstance');
   }
 }
